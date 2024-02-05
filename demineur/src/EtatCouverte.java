ad871470ebd8b7cf187f880db266b9ccbe2d8751
Fares <58696751+Tavolata@users.noreply.github.com>
2024-02-05 18:41:22 +0100

    Ajout Exdeptions

--- demineur/src/EtatCouverte.java
+++ demineur/src/EtatCouverte.java
@@ -1,12 +1,18 @@
 public class EtatCouverte extends EtatCase {
     public void decouvrir(Case c){
-        if (c.getEtatCourant() == 0){
-            c.setEtatCourant();
+        if (c.getEtatCourant() == 0 && c.getEtatCourant()==1){
+            c.setEtatCourant(2);
         }
-
-        
+        else if (c.getEtatCourant() == 2){
+            Exception e = new Exception("Case déjà découverte");
+        }
     }
     public void marquer(Case c){
-        c.setEtatCourant(c.getEtatCourant());
+        if (c.getEtatCourant() == 0){
+            c.setEtatCourant(1);
+        }
+        else if (c.getEtatCourant() == 1){
+            c.setEtatCourant(0);
+        }
     }
 }
