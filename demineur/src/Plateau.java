32aa66d91f4486a9044881a5c6125ed78fb3a756
Kari13cyber <131266205+Kari13cyber@users.noreply.github.com>
2024-02-06 11:36:31 +0100

    V2

--- demineur/src/Plateau.java
+++ demineur/src/Plateau.java
@@ -1,24 +1,33 @@
 import java.awt.*;
 
 public class Plateau extends Partie{
-    private HashMap<Coordonnees,Case> desCase;
+    private HashMap<Coordonnées,Case> desCase;
     private int largeur=9;
     private int hauteur=9;
-
     public Plateau(String niveau) {
         super(niveau);
     }
 
     public boolean testerSiGagne(){
-        return(Partie.resultat);
+        boolean s= super.resultat;
+        return(s);
     }
+    @Override
     public void marquerCase(Point pt){
-
+        super.marquerCase(pt);
     }
+    @Override
     public void decouvrirCase(Point pt){
-
+        super.decouvrirCase(pt);
     }
+    @Override
     public void afficher(){
+        super.afficher();
 
+        for(int i=0;i<largeur;i++){
+            for(int j=0;j<hauteur;j++){
+
+            }
+        }
     }
 }
